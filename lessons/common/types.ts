3c7d5bf236452324deb9edf728372582bda92f28
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2019-10-05 15:47:02 +0200

    Base types are ready

---
+++ lessons/common/types.ts
@@ -0,0 +1,40 @@
+export interface Size {
+  width: number;
+  height: number;
+}
+
+export enum LayerType {
+  Text,
+  Image
+}
+
+export interface Position {
+  x: number;
+  y: number;
+}
+
+export interface Layer {
+  id: string;
+  type: LayerType;
+  opacity: number;
+  rotation: number;
+  center: Position;
+  size: Size;
+}
+
+export interface TextLayer extends Layer {
+  type: LayerType.Text;
+
+  text: string;
+  color: string;
+}
+
+export interface ImageLayer extends Layer {
+  type: LayerType.Image;
+  src: string;
+}
+
+export interface Project {
+  layers: Layer[];
+  size: Size;
+}

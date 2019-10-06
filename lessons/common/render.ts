e9e15e6a649812e4a059f9293f8296133fbf6b00
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2019-10-06 13:37:17 +0200

    Basic hardcoded rendering with image open working

--- lessons/common/render.ts
+++ lessons/common/render.ts
@@ -1,6 +1,8 @@
 import { Project } from "./types";
 import { createCanvas, loadImage } from "canvas";
 import process from "process";
+import fs from "fs";
+import open from "open";
 
 export function render(project: Project) {
   const root = process.env.ROOT;
@@ -9,23 +11,17 @@ export function render(project: Project) {
   const canvas = createCanvas(200, 200);
   const ctx = canvas.getContext("2d");
 
-  // Write "Awesome!"
-  ctx.font = "30px Impact";
-  ctx.rotate(0.1);
+  ctx.font = "10px Impact";
   ctx.fillText("Awesome!", 50, 100);
 
-  // Draw line under text
-  var text = ctx.measureText("Awesome!");
-  ctx.strokeStyle = "rgba(0,0,0,0.5)";
-  ctx.beginPath();
-  ctx.lineTo(50, 102);
-  ctx.lineTo(50 + text.width, 102);
-  ctx.stroke();
-
-  // Draw cat with lime helmet
   loadImage(`${imagePath}/ps-dark.png`).then(image => {
     ctx.drawImage(image, 50, 0, 70, 70);
 
-    console.log('<img src="' + canvas.toDataURL() + '" />');
+    const fileName = __dirname + "/test.png";
+
+    const out = fs.createWriteStream(fileName);
+    const stream = canvas.createPNGStream();
+    stream.pipe(out);
+    out.on("finish", () => open(fileName));
   });
 }
